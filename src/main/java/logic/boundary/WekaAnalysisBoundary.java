184b1cfbf4fa4f9f625f926a15fee79eb3ace5be
lucaMastro <luca.mastrobattista@students.uniroma2.eu>
2023-02-08 19:17:50 +0100

    removed smell

--- src/main/java/logic/boundary/WekaAnalysisBoundary.java
+++ src/main/java/logic/boundary/WekaAnalysisBoundary.java
@@ -3,10 +3,6 @@ package logic.boundary;
 import logic.bean.WekaBean;
 import logic.controller.WekaController;
 
-import java.util.Arrays;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class WekaAnalysisBoundary {
 
     String csvInputFile;
@@ -29,10 +25,6 @@ public class WekaAnalysisBoundary {
             bean = new WekaBean(this.csvInputFile, this.csvOutputFile, this.arfOutputFile);
             var controller = new WekaController();
             controller.run(bean);
-        }catch (Exception e){
-            var logger = Logger.getLogger(WekaController.class.getName());
-            logger.log(Level.OFF, Arrays.toString(e.getStackTrace()));
-            throw e;
         }
         finally {
             if (bean != null)
